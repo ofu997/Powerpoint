c964b5f8c75a68d78798a25f6005b809a54ff607
oliver fu <ofu997@gmail.com>
2018-04-25 02:07:32 -0700

    + Url generator and styling

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-// added this one
+// added these
 using Microsoft.Office.Interop.PowerPoint;
 using Microsoft.Office.Core;
 
@@ -16,7 +15,6 @@ namespace Powerpoint
         Title area 
         Text area 
         and a image suggestion are that utilizes words in the title, and bold words in the text are to bring suggested images in, with ability to select multiple images to included in the slide 
-
         Have them make windows form to provide the solution. 
     */
 
@@ -30,53 +28,40 @@ namespace Powerpoint
             string Text = "";
             string ImageURL = "";
             Console.WriteLine("Enter the name of the slide title, please.");
-            Title+=Console.ReadLine().ToString();
-            Console.WriteLine("Enter slide description, please.");
+            Title += Console.ReadLine().ToString();
+            Console.WriteLine("Enter image search key words, please.");
             Text += Console.ReadLine().ToString();
-            ImageURL += "https://www.google.com/search?q=" + Text + "&tbm=isch";
-
-            // string[] PictureFile = { @"C:\powerpoint\img.1.jpg", @"C:\powerpoint\img.1.jpg", @"C:\powerpoint\img.1.jpg" };
-
+            
             Application pptApplication = new Application();
-
-            // Creates presentation 
             Presentation pptpresentation = pptApplication.Presentations.Add(MsoTriState.msoTrue);
-            //for (int i = 0; i < 1; i++)
-            //{
-                Microsoft.Office.Interop.PowerPoint.Slides slides;
-                Microsoft.Office.Interop.PowerPoint._Slide slide;
-                Microsoft.Office.Interop.PowerPoint.TextRange objText;
+
+            Microsoft.Office.Interop.PowerPoint.Slides slides;
+            Microsoft.Office.Interop.PowerPoint._Slide slide;
+            
             // Microsoft.Office.Interop.PowerPoint.TextFrame textFrame;
 
-                Microsoft.Office.Interop.PowerPoint.CustomLayout custLayout=
-                    pptpresentation.SlideMaster.CustomLayouts[Microsoft.Office.Interop.PowerPoint.PpSlideLayout.ppLayoutText];
-                
-                // Creates new slide
-                slides = pptpresentation.Slides;
-                slide = slides.AddSlide(1, custLayout);
+            Microsoft.Office.Interop.PowerPoint.CustomLayout custLayout =
+                pptpresentation.SlideMaster.CustomLayouts[Microsoft.Office.Interop.PowerPoint.PpSlideLayout.ppLayoutText];
 
-                objText = slide.Shapes[1].TextFrame.TextRange;
-                objText.Text = Text ;
-                objText.Font.Name = "Book Angigua";
-                objText.Font.Size = 32;
-            // TRY TO INSERT TEXT BOX IN SLIDE 
+            // Creates new slide
+            slides = pptpresentation.Slides;
+            slide = slides.AddSlide(1, custLayout);
 
-            slide.Shapes.AddTextbox(MsoTextOrientation.msoTextOrientationHorizontal,
-                Left: 0,
-                Top: 0,
-               Width: 0,
-               Height: 0
-                )
-                .TextFrame.TextRange.Text = Text; 
+            Microsoft.Office.Interop.PowerPoint.TextRange objText;
+            objText = slide.Shapes[1].TextFrame.TextRange;
+            objText.Font.Size = 55;
+            objText.Text = Title;
+
+            Microsoft.Office.Interop.PowerPoint.TextRange objText2;
+            objText2= slide.Shapes[2].TextFrame.TextRange;
+            ImageURL += "https://www.google.com/search?q=" + Text + "&tbm=isch";
+            objText2.Text = ImageURL;
+                objText2.Font.Name = "Book Angigua";
+                objText2.Font.Size = 12;
+                objText2.ActionSettings[Microsoft.Office.Interop.PowerPoint.PpMouseActivation.ppMouseClick].Hyperlink.Address = ImageURL;
             
-
-                //Microsoft.Office.Interop.PowerPoint.Shape shape =
-                   // slide.Shapes[2];
-                // slide.Shapes.AddPicture(PictureFile[i], Microsoft.Office.Core.MsoTriState.msoFalse,
-                    // Microsoft.Office.Core.MsoTriState.msoTrue, shape.Left, shape.Top, shape.Width, shape.Height);
-            // }
-            pptpresentation.SaveAs("newslide.pptx", 
-                Microsoft.Office.Interop.PowerPoint.PpSaveAsFileType.ppSaveAsDefault, 
+            pptpresentation.SaveAs("newslide.pptx",
+                Microsoft.Office.Interop.PowerPoint.PpSaveAsFileType.ppSaveAsDefault,
                 Microsoft.Office.Core.MsoTriState.msoTrue);
         }
     }
